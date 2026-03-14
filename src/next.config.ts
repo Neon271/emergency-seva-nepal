efb2060a1f0733f9a005e77d7da96079de2dc517
Prajwol bohora <prajwolbohora74@gmail.com>
2026-03-14 11:35:19 +0000

    there is proble m in  android/app/src/main/assets/public/index.html

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const withPWA = require('next-pwa')({
-    dest: 'public',
+    dest: 'out',
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development'
